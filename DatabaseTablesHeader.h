247f3d06332ef764d4d66838a547fd61ee2008b2
Heena Rastogi <rastogi.heena@gmail.com>
2012-02-22 15:28:25 -0800

    Added db files

---
+++ DatabaseTablesHeader.h
@@ -0,0 +1,17 @@
+/*
+ *  DatabaseTablesHeader.h
+ *  MisoUIV3
+ *
+ *  Created by Heena Rastogi on 9/7/11.
+ *  Copyright 2011 Miso Media Inc. All rights reserved.
+ *
+ */
+
+#import "song_description.h"
+#import "user_info.h"
+#import "tablature_description.h"
+#import "instruments_description.h"
+#import "accumulative_free_content.h"
+#import "migration_version.h"
+#import "users.h"
+#import "user_info.h"
